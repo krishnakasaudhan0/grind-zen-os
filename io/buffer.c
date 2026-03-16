d08a57a02325b529662d7084b5ee89f22e54b412
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-03-16 10:05:32 +0000

    fix: resolve buffer overflow in system I/O

--- io/buffer.c
+++ io/buffer.c
@@ -2,3 +2,5 @@
 // Git contribution logic update at: 2026-03-15T05:53:06.000Z [hash: i582w2]
 
 // Git contribution logic update at: 2026-03-15T12:29:45.000Z [hash: ozdn7]
+
+// Git contribution logic update at: 2026-03-16T10:05:32.000Z [hash: 9vryt2]
